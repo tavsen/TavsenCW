4246268e73e685da14df5840505685bed09e7321
Zvarich Svyatoslav <71327349+tavsen@users.noreply.github.com>
2022-04-14 13:55:17 +0300

    Update test_AA1.py

--- test_AA1.py
+++ test_AA1.py
@@ -10,7 +10,7 @@ class AATest(unittest.TestCase):
         self.AA.add(12)
         self.assertEqual(self.AA.root.val, 12)
 
-    def test_addnval_neg(self):
+    def test_addval_neg(self):
         self.AA.add(-12)
         self.assertEqual(self.AA.root.val, -12)
 
@@ -18,21 +18,11 @@ class AATest(unittest.TestCase):
         self.AA.add(12.3)
         self.assertEqual(self.AA.root.val, 12.3)
 
-    def test_addval_none(self):
-        self.AA.add(None)
-        self.assertEqual(self.AA.root.val, None)
-    # def test_backward_from(self):
-    #     self.AA.add(12)
-    #     self.AA.add(23)
-    #     self(*[v for v in self.forward_from(2))
-    #     self.assertEqual(self.AA.backward_from(), 23, 12)
-
-    # def test_addval_multiple(self):
-    #     self.AA.add(10)
-    #     self.AA.add(13)
-    #     self.AA.add(12)
-    #     self.assertEqual(self.AA.val, 10, self.AA.val,12, 13)
-
+    def test_addval_mul(self):
+        self.AA.add(12)
+        self.AA.add(23)
+        self.assertEqual(self.AA.root.val, 12)
+        self.assertEqual(self.AA.root.right.val, 23)
 
 
 if __name__ == '__main__':
