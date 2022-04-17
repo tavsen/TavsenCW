d83cb9f118b31cd8d54d620b48866e3d9b81bbac
Zvarich Svyatoslav <71327349+tavsen@users.noreply.github.com>
2022-04-17 23:32:20 +0300

    Update guy.py

--- guy.py
+++ guy.py
@@ -5,7 +5,7 @@ import sys
 import logging
 
 logging.basicConfig(level=logging.INFO)
-#logging.disable(logging.INFO)
+
 
 
 class MainWindow(QMainWindow):
@@ -21,157 +21,70 @@ class MainWindow(QMainWindow):
         self.ui.btn_save.clicked.connect(lambda: self.Facade.save_data())
 
 
-
     def closeEvent(self, event):
         if self.Facade.data_wait_for_save:
-            logging.log(logging.INFO, ' есть несохраненные данные')
-            self.save.setWindowTitle("Несохраненные данные")
-            self.save.setText("Остались несохраненные данные!")
-            self.save.setInformativeText("Хотите их сохранить?")
-
+            logging.log(logging.INFO, ' You have unsaved Data')
+            self.save.setWindowTitle("Warning")
+            self.save.setText("You have got unsaved data!")
+            self.save.setInformativeText("Would you like to save it?")
             self.save.setIcon(QMessageBox.Question)
             self.save.setStandardButtons(QMessageBox.Cancel | QMessageBox.Save)
-
             self.save.buttonClicked.connect(self.action)
             self.save.exec_()
         else:
-            logging.log(logging.INFO, ' несохраненных данных нет')
+            logging.log(logging.INFO, ' There is no unsaved data')
 
-        logging.log(logging.INFO, ' окно закрыто')
+        logging.log(logging.INFO, ' Window closed')
 
     def action(self, btn):
         if btn.text() == "Save":
-            logging.log(logging.INFO, " данные сохранены")
+            logging.log(logging.INFO, " Data saved ")
             self.Facade.save_data()
         else:
-            logging.log(logging.INFO, " данные не сохранены")
+            logging.log(logging.INFO, " Data unsaved ")
 
     def open_dialog_input(self):
         dialog = DialogInput(self.Facade, self)
-        dialog.setWindowTitle("add val")
+        dialog.setWindowTitle("Add Data")
         dialog.show()
 
     def open_dialog_delete(self):
-        dialog = DialogDelete(self.Facade, self)  # тут self. нужен для теста (чтобы можно было обратиться к dialog)
-        dialog.setWindowTitle("Удаление данных")
+        dialog = DialogDelete(self.Facade, self)
+        dialog.setWindowTitle("Delete Data")
         dialog.show()
 
-    def draw_el(self, x, y, val, left=None, height=0):
-        self.scene.addEllipse(x, y, 40, 40)
-        long = len(str(val))
-        text = self.scene.addText(f"{val}")
-        text.moveBy(x + 17 - long * 3, y + 40)
-
-        if left == 1:
-            self.scene.addLine(x + 30, y - 10, x + self.branch_len + 15, y - height + 60)
-        if left == 0:
-            self.scene.addLine(x + 10, y - 10, x - self.branch_len + 25, y - height + 60)
-
-    def draw_tree(self):
-        self.scene.clear()
-        path = self.Facade.bypass_tree()
-
-        x = 50 * (2 ** path[0]) + 50
-        y = 150
-        h = 50
-        height = h * path[0] + h
-        self.branch_len = (x - 50) // 2
-        layer = 0
-        frame_x = 0
-        for val in range(1, path[0] + 2):
-            frame_x += h * val
-
-        self.scene = QGraphicsScene(0, 0, x * 2 + 100, frame_x + 300)
-        logging.log(logging.INFO, f' размер холста - {x * 2 + 100}, {y * path[0] + 100}')
-        self.ui.canvas.setScene(self.scene)
-
-        if len(path) != 1:
-            self.draw_el(x, y, path[1][0], path[1][1])
-
-        for n in range(2, len(path)):
-            if path[n][1] is not None:
-                y += height
-                layer += 1
-
-                if path[n - 1][0] > path[n][0]:
-                    x -= self.branch_len
-                    self.draw_el(x, y, path[n][0], path[n][1], 1, height)
-
-                else:
-                    x += self.branch_len
-                    self.draw_el(x, y, path[n][0], path[n][1], 0, height)
-                self.branch_len //= 2
-
-                height -= h
-
-            else:
-                height += h
-                layer -= 1
-                y -= height
-                self.branch_len *= 2
-                if path[n - 1][0] < path[n][0]:
-                    x += self.branch_len
-                else:
-                    x -= self.branch_len
 
 class DialogDelete(QDialog):
-    def __init__(self, Facade, parent=None):
-        super(DialogDelete, self).__init__(parent)
+    def __init__(self, Facade, val=None):
+        super(DialogDelete, self).__init__(val)
         self.Facade = Facade
         self.ui = uic.loadUi("forms/delete.ui", self)
         self.ui.btn_remove.clicked.connect(self.delete)
         self.ui.btn_remove_all.clicked.connect(lambda: self.del_all())
 
-    def del_all(self):
-        if self.Facade.dictionary.key is not None:  # если есть данные для удаления
-            self.messagebox_del_all = QMessageBox(self)
-            self.messagebox_del_all.setWindowTitle("Удаление данных")
-            self.messagebox_del_all.setText("Вы уверены, что хотите удалить все данные?")
-            self.messagebox_del_all.setInformativeText("После сохранения данные будут утеряны!")
-
-            self.messagebox_del_all.setIcon(QMessageBox.Question)
-            self.messagebox_del_all.setStandardButtons(QMessageBox.Cancel | QMessageBox.Ok)
-
-            self.messagebox_del_all.buttonClicked.connect(self.action)
-            self.messagebox_del_all.show()
-        else:
-            QMessageBox.warning(self, 'Удаление данных', "Данных для удаления нет", QMessageBox.Ok)
-
     def action(self, button):
         if button.text() == "OK":
             self.Facade.del_all_from_sbst()
             self.parent().draw_tree()
-            logging.log(logging.INFO, 'Value deleted')
+            logging.log(logging.INFO, 'Data deleted')
         else:
-            logging.log(logging.INFO, 'There is no deleted data')
+            logging.log(logging.INFO, 'Data is not deleted')
 
     def delete(self):
-        link = self.Facade.search_element_in_tree(self.ui.input_key.value())  # ссылка на элемент, который удаляем
+        link = self.Facade.remove_value
         if link is not None:
             try:
-                self.ui.label_info.setText(f"Вы удалили: {self.ui.input_key.value()}, {link.data}")
-                self.Facade.delete_value(self.ui.input_key.value())
-                self.parent().draw_tree()
+                self.ui.label_info.setText(f"You deleted:{link.remove()}")
+                self.Facade.remove_value()
             except RecursionError:
-                self.ui.label_info.setText(f"Tree is too high!")
+                self.ui.label_info.setText(f"Tree is too high")
 
 
 class DialogInput(QDialog):
-
-
-    def __init__(self, Facade, parent=None):
+    def __init__(self, Facade, val=None):
         self.Facade = Facade
-        super(DialogInput, self).__init__(parent)
+        super(DialogInput, self).__init__(val)
         self.ui = uic.loadUi("forms/input.ui", self)
-        self.ui.btn_insert.clicked.connect(self.add)
-    def add(self):
-        if self.ui.input_value() is None:
-            try:
-                self.Facade.insert_value(self.ui.val_input.text())
-                self.parent().draw_tree()
-                logging.log(logging.INFO, 'заполните поля')
-            except RecursionError:
-                self.ui.label_info.setText(f"Дерево слишком высокое!")
 
 
 class Builder:
@@ -199,4 +112,5 @@ if __name__ == '__main__':
     builder.create_gui()
     window = builder.get_result()
     window.show()
+
     qapp.exec()
